e364fbc6000dbaeb0ecef7a96b3ef290b758c228
Daniel Bartolini <djb3149@gmail.com>
2023-01-08 13:55:19 -0500

    added first queries

--- server.js
+++ server.js
@@ -8,21 +8,46 @@ const db = mysql.createConnection({
     database: 'employee_db'
 });
 
-switch(type) {
-    case 'VIEW ALL EMPLOYEES': {
-        db.query('SELECT * FROM employee', (err, employees) => {
-            console.table(employees);
-        });
-    }
-    case 'VIEW ALL DEPARTMENTS': {
-        db.query('SELECT * FROM department', (err, departments) => {
-            console.table(departments);
-        });
-    }
-    case 'VIEW ALL ROLES': {
-        db.query('SELECT * FROM role', (err, roles) => {
-            console.table(roles);
-        });
+const chooseOption = (type) => {
+    switch(type) {
+        case 'VIEW ALL EMPLOYEES': {
+            db.query('SELECT * FROM employee', (err, employees) => {
+                console.table(employees);
+                init();
+            });
+            break;
+        }
+        case 'VIEW ALL DEPARTMENTS': {
+            db.query('SELECT * FROM department', (err, departments) => {
+                console.table(departments);
+                init();
+            });
+            break;
+        }
+        case 'VIEW ALL ROLES': {
+            db.query('SELECT * FROM role', (err, roles) => {
+                console.table(roles);
+                init();
+            });
+            break;
+        }
     }
 }
 
+const init = () => {
+    prompt({
+        type: 'rawlist',
+        message: 'Choose one:',
+        choices: [
+            'VIEW ALL EMPLOYEES',
+            'VIEW ALL DEPARTMENTS',
+            'VIEW ALL ROLES'
+        ],
+        name: 'type'
+    })
+    .then((answers) => {
+        chooseOption(answers.type);
+    })
+};
+
+init();
